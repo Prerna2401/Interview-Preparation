7158e2f513961b626b4b9771e8ff0199f0a4dca1
Prerna2401 <83159821+Prerna2401@users.noreply.github.com>
2023-07-12 16:19:13 +0530

    Delete ReverseInt.py

--- Strings/ReverseInt.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def reverse(self, x: int) -> int:
-        rev = 0
-        while x != 0:
-            y = x % 10
-            rev = rev*10+y
-            x = x // 10
-        return rev
