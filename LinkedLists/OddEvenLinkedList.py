1f4c87602ceab53e8ed994374ca46b912f80b376
Prerna <83159821+Prerna2401@users.noreply.github.com>
2023-07-23 05:18:13 +0530

    Add files via upload

---
+++ LinkedLists/OddEvenLinkedList.py
@@ -0,0 +1,17 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head is None or head.next is None: return head
+        odd, even = head, head.next
+        temp = head.next
+        while even and even.next:
+            odd.next = odd.next.next
+            even.next = even.next.next
+            odd = odd.next
+            even = even.next
+        odd.next = temp
+        return head
